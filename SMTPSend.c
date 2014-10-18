c53de05bfc15dc99079f7053fe534e9bbdba038d
I Putu Pradnyana <pradnyana12@mhs.if.its.ac.id>
2014-10-19 00:45:01 +0700

    Now can send email, but authentication still not supported

--- SMTPSend.c
+++ SMTPSend.c
@@ -9,8 +9,10 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#include <time.h>
+
 int main(int argc, char **argv){
-    char domain[512];
+    char *domain = (char*)malloc(sizeof(char) * 512);
     //Getting the input from user
     /***************************************************************************/
     printf("Please insert the mail server domain :\n");
@@ -53,7 +55,7 @@ int main(int argc, char **argv){
     
     //Trying to connect to the server
     int tries = 0;
-    char *address;
+    char *address = (char*)malloc(sizeof(char) * 30);
     //Loop through all the result
     for(iter = servaddr; iter != NULL; iter->ai_next){
         ++tries;
@@ -61,12 +63,13 @@ int main(int argc, char **argv){
         //Check the IP version
         if(iter->ai_family == AF_INET){
             sockfd = sockfd4;
-            h1 = (struct sockaddr_in *)h;
-            inet_ntop(AF_INET, &(h1)->sin_addr, address, sizeof(h1->sin_addr));            
+            h1 = (struct sockaddr_in*)h;
+            inet_ntop(AF_INET, &(h1->sin_addr), address, sizeof(h1->sin_addr));
         }
         else if(iter->ai_family == AF_INET6){
             sockfd = sockfd6;
-            inet_ntop(AF_INET6, &(h2)->sin6_addr, address, sizeof(h2->sin6_addr));
+            h2 = (struct sockaddr_in6*)h;
+            inet_ntop(AF_INET6, &(h2->sin6_addr), address, sizeof(h2->sin6_addr));
         }
         
         fprintf(stdout, "Connecting to %s\n", address);
@@ -79,6 +82,7 @@ int main(int argc, char **argv){
     }
     //Deallocate the unused memory
     freeaddrinfo(servaddr);
+    free(address);
     /***************************************************************************/
     
     //Interacting with the server
@@ -90,25 +94,127 @@ int main(int argc, char **argv){
     //Message size
     int msgSize;
     
-    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer));
-    fprintf(stdout, "%s", recvBuffer);
+    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+    recvBuffer[msgSize] = '\0';
+//    fprintf(stdout, "%s", recvBuffer);
     
     //Send our hello to the server
-    sprintf(sendBuffer, "ehlo %s\n", domain);
+    sprintf(sendBuffer, "EHLO %s\n", domain);
     write(sockfd, sendBuffer, strlen(sendBuffer));
     
-    //Print the response
-    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer)); 
-    fprintf(stdout, "%s", recvBuffer);
+    //Get the response
+    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+    recvBuffer[msgSize] = '\0';
+//    fprintf(stdout, "%s", recvBuffer);
 
-    memset(sendBuffer, 0, sizeof(sendBuffer));
-    memset(recvBuffer, 0, sizeof(recvBuffer));
+    free(domain);
+    //Sending e-mail
+    /***************************************************************************/
+    int result;
+    char *mail = (char*)malloc(sizeof(char) * 50);
+    //Specify the sender
+    do{
+        //Get the sender
+        fprintf(stdout, "Specify the sender : ");
+        scanf("%s", mail);
+        sprintf(sendBuffer,"MAIL FROM: %s\n",mail);
+        
+        write(sockfd, sendBuffer, strlen(sendBuffer));
+        
+        //Print the response
+        msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+        recvBuffer[msgSize] = '\0';
+//        fprintf(stdout, "%s", recvBuffer);
+        result = strcmp(recvBuffer, "250 2.1.0 Ok\r\n");
+    }while(result != 0);
+    
+    char *sender = (char*)malloc(sizeof(char) * 50);
+    strcpy(sender, mail);
+    
+    //Specify the receiver
+    do{
+        //Get the receiver
+        fprintf(stdout, "Specify the recipient : ");
+        scanf("%s", mail);
+        sprintf(sendBuffer,"RCPT TO: %s\n",mail);
+        
+        write(sockfd, sendBuffer, strlen(sendBuffer));
+        
+        //Print the response
+        msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+        recvBuffer[msgSize] = '\0';
+//        fprintf(stdout, "%s", recvBuffer);
+        result = strcmp(recvBuffer, "250 2.1.5 Ok\r\n");
+        if(result != 0) fprintf(stderr, "There is no such address in this server\n");
+    }while(result != 0);
+        
+    char *reciever = (char*)malloc(sizeof(char) * 50);
+    strcpy(reciever, mail);
+    
+    free(mail);
+    
+    //Write data
+    write(sockfd, "DATA\n", strlen("DATA\n"));
+    
+    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+    recvBuffer[msgSize] = '\0';
+//    fprintf(stdout, "%s", recvBuffer);
+    
+    //Get current time
+    char *dates = (char*)malloc(sizeof(char) * 512);
+    time_t t = time(NULL);
+    struct tm *times = localtime(&t);
+    strftime(dates, sizeof(dates), "%a, %d %B %Y %T %z", times);
+    fprintf(stdout, "%s\n", dates);
+    
+    //Write the date
+    sprintf(sendBuffer, "Date: %s\n", dates);
+    write(sockfd, sendBuffer, strlen(sendBuffer));
+    
+    //Write the sender
+    sprintf(sendBuffer, "From: %s\n", sender);
+    write(sockfd, sendBuffer, strlen(sendBuffer));
+    
+    //Write the subject
+    fprintf(stdout, "Subject : ");
+    fgets(sendBuffer, sizeof(sendBuffer), stdin);
+    
+    write(sockfd, "Subject: ", strlen("Subject: "));
+    write(sockfd, sendBuffer, strlen(sendBuffer));
+    
+    //Write the recipient
+    sprintf(sendBuffer, "To: %s\n", reciever);
+    write(sockfd, sendBuffer, strlen(sendBuffer));
+    
+    free(dates);
+    free(sender);
+    free(reciever);
+    //Write the message
+    /***************************************************************************/
+    fprintf(stdout, "Write your message below\n");
+    do{
+        fgets(sendBuffer, sizeof(sendBuffer), stdin);
+        write(sockfd, sendBuffer, strlen(sendBuffer));
+        result = strcmp(sendBuffer, ".\n");
+    } while (result != 0);
+
+     msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+    recvBuffer[msgSize] = '\0';
+    fprintf(stdout, "%s", recvBuffer);
+    
+    /***************************************************************************/
+    
+    
+    /***************************************************************************/
+    
+    
     //Exit from the server
-    sprintf(sendBuffer, "quit\n");
+    sprintf(sendBuffer, "QUIT\n");
     write(sockfd, sendBuffer, strlen(sendBuffer));
     
      //Print the response
-    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer));
+    msgSize = read(sockfd, recvBuffer, sizeof(recvBuffer) - 1);
+    recvBuffer[msgSize] = '\0';
     fprintf(stdout, "%s", recvBuffer);
     
     /***************************************************************************/
